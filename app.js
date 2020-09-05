97b0e5c33042715a705442e97434cf7631140a9e
王杰强 <joker@MacBookPro.local>
2020-09-05 16:45:10 +0800

    fix refresh issue

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ server.listen(6001, function () {
 });
 
 server.use('/', express.static('./'));
+server.use('/about', express.static('./'));
+server.use('/ipLicensing', express.static('./'));
+server.use('/showcase', express.static('./'));
+server.use('/contact', express.static('./'));
+server.use('/showcase/fashion', express.static('./'));
+server.use('/showcase/accessories', express.static('./'));
+server.use('/showcase/catering', express.static('./'));
+server.use('/showcase/household', express.static('./'));
+server.use('/showcase/toys', express.static('./'));
