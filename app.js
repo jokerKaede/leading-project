0ee0c25d5ba7a7794a38bebee231b65188caa10d
kaede <441895454@qq.com>
2021-03-16 22:06:47 +0800

    update 2021-03-16

--- app.js
+++ app.js
@@ -25,14 +25,7 @@ schedule.scheduleJob('0 0 0 * * *', function() {
 
 server.all('*', function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
-    if(req.method=="OPTIONS") {
-        res.send(200); 
-    } else {
-        next();
-    }
+    next();
 });
 
 server.post('/sendMail', function(req, res) {
